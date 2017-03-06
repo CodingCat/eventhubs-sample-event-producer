857440c97ead54527e0f1863f3d1c970126fb3f0
Nan Zhu <zhunansjtu@gmail.com>
2017-03-06 08:01:05 -0800

    test

--- src/main/scala/com/microsoft/azure/eventhubs/client/example/SimpleProducer.scala
+++ src/main/scala/com/microsoft/azure/eventhubs/client/example/SimpleProducer.scala
@@ -1,5 +1,7 @@
 package com.microsoft.azure.eventhubs.client.example
 
+import scala.collection.mutable.ListBuffer
+
 import com.microsoft.azure.eventhubs.{EventData, EventHubClient}
 import com.microsoft.azure.servicebus.ConnectionStringBuilder
 
@@ -16,10 +18,15 @@ class SimpleProducer (val policyName: String,
   val eventHubsClient: EventHubClient = EventHubClient.createFromConnectionString(connectionString.toString).get
 
   def run(): Unit = {
+    import scala.collection.JavaConverters._
     for (i <- 0 until sendCount) {
-      val eventPayLoad = Array.fill(eventLength)('a').mkString
-      val eventData = new EventData(eventPayLoad.getBytes)
-      eventHubsClient.sendSync(eventData)
+      val buffer = new ListBuffer[EventData]
+      if (i % 1000 == 0) {
+        val eventPayLoad = Array.fill(eventLength)('a').mkString
+        val eventData = new EventData(eventPayLoad.getBytes)
+        buffer += eventData
+        eventHubsClient.sendSync(buffer.asJava)
+      }
     }
   }
 }
