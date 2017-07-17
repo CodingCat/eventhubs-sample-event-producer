1d19629b7cb026aec39bd43efccaa80e194d0a72
Nan Zhu <zhunansjtu@gmail.com>
2017-07-17 09:30:44 -0700

    pass in filter param

--- src/main/scala/com/microsoft/azure/eventhubs/client/example/SimpleReceiver.scala
+++ src/main/scala/com/microsoft/azure/eventhubs/client/example/SimpleReceiver.scala
@@ -1,5 +1,7 @@
 package com.microsoft.azure.eventhubs.client.example
 
+import java.time.Instant
+
 import com.microsoft.azure.eventhubs.EventHubClient
 import com.microsoft.azure.servicebus.ConnectionStringBuilder
 
@@ -13,11 +15,19 @@ object SimpleReceiver {
     val eventHubName = args(3)
     val partition = args(4)
     val receiveMaxCount = args(5).toInt
+    val epochTimeOrOffset = args(6).toLong
+    val filterWithOffset = args(7).toBoolean
 
     val connectionString = new ConnectionStringBuilder(eventHubNamespace, eventHubName,
       policyName, policyKey)
     val client  = EventHubClient.createFromConnectionStringSync(connectionString.toString)
-    val receiver = client.createReceiverSync("$Default", s"$partition", "-1")
+    val receiver = {
+      if (filterWithOffset) {
+        client.createReceiverSync("$Default", s"$partition", epochTimeOrOffset.toString)
+      } else {
+        client.createReceiverSync("$Default", s"$partition", Instant.ofEpochSecond(epochTimeOrOffset))
+      }
+    }
     for (i <- 0 until receiveMaxCount) {
       val eventDatas = receiver.receiveSync(1)
       eventDatas.asScala.foreach(eventData =>
