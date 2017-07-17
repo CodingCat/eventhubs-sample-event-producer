852c719cbccd1f075c2a21c5de8c609304f5ee6b
Nan Zhu <zhunansjtu@gmail.com>
2017-07-17 09:15:48 -0700

    test receiver

--- src/main/scala/com/microsoft/azure/eventhubs/client/example/SimpleReceiver.scala
+++ src/main/scala/com/microsoft/azure/eventhubs/client/example/SimpleReceiver.scala
@@ -5,7 +5,6 @@ import com.microsoft.azure.servicebus.ConnectionStringBuilder
 
 object SimpleReceiver {
 
-
   def main(args: Array[String]): Unit = {
     import scala.collection.JavaConverters._
     val policyName = args(0)
@@ -13,14 +12,18 @@ object SimpleReceiver {
     val eventHubNamespace = args(2)
     val eventHubName = args(3)
     val partition = args(4)
+    val receiveMaxCount = args(5).toInt
 
     val connectionString = new ConnectionStringBuilder(eventHubNamespace, eventHubName,
       policyName, policyKey)
     val client  = EventHubClient.createFromConnectionStringSync(connectionString.toString)
     val receiver = client.createReceiverSync("$Default", s"$partition", "-1")
-    for (i <- 0 until 100) {
-      val eventDatas = receiver.receiveSync(100)
-      eventDatas.asScala.foreach(eventData => println(eventData.getBody.length))
+    for (i <- 0 until receiveMaxCount) {
+      val eventDatas = receiver.receiveSync(1)
+      eventDatas.asScala.foreach(eventData =>
+        println(
+          s"${eventData.getSystemProperties.getEnqueuedTime}," +
+            s" ${eventData.getSystemProperties.getOffset}, ${eventData.getSystemProperties.getSequenceNumber}"))
     }
 
   }
